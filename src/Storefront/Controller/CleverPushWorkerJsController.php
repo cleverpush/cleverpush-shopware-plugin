257135a261fbc6f095ba31924566bae2f1512c86
Marcus Müller <25648755+M-arcus@users.noreply.github.com>
2025-08-04 09:46:11 +0200

    Update plugin to 6.6 and make 6.7 compatible (#2)
    * Update structure of plugin
    
    * Update plugin to 6.6 and make 6.7 compatible

--- src/Storefront/Controller/CleverPushWorkerJsController.php
+++ src/Storefront/Controller/CleverPushWorkerJsController.php
@@ -2,16 +2,14 @@
 
 namespace CleverPush\CleverPushShopware\Storefront\Controller;
 
-use Shopware\Storefront\Framework\Cache\Annotation\HttpCache;
-use Shopware\Storefront\Controller\StorefrontController;
+use Shopware\Core\PlatformRequest;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
+use Shopware\Storefront\Controller\StorefrontController;
+use Shopware\Storefront\Framework\Routing\StorefrontRouteScope;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Routing\Attribute\Route;
 
-/**
- * @Route(defaults={"_routeScope"={"storefront"}})
- */
+#[Route(defaults: [PlatformRequest::ATTRIBUTE_ROUTE_SCOPE => [StorefrontRouteScope::ID]])]
 class CleverPushWorkerJsController extends StorefrontController
 {
     private SystemConfigService $systemConfigService;
@@ -21,18 +19,19 @@ class CleverPushWorkerJsController extends StorefrontController
         $this->systemConfigService = $systemConfigService;
     }
 
-    /**
-     * @HttpCache()
-     * @Route("/cleverpush/cleverpush-worker.js", name="frontend.example.example", methods={"GET"})
-     */
+    #[Route(path: '/cleverpush-worker.js', name: 'frontend.cleverpush.worker', defaults: ['_httpCache' => true], methods: ['GET'])]
     public function showCleverPushWorkerJs(): Response
     {
-        $channelId = $this->systemConfigService->get('CleverPushShopwarePlugin.config.channelId');
-        $workerContent = 'importScripts("https://static.cleverpush.com/channel/worker/' . $channelId . '.js");';
-        return new Response(
-            $workerContent,
-            Response::HTTP_OK,
-            ['content-type' => 'text/javascript']
-        );
+        $channelId = trim($this->systemConfigService->getString('CleverPushShopwarePlugin.config.channelId'));
+
+        $content = 'console.log(\'CleverPush Channel ID is not configured.\');';
+        if ($channelId !== '' && $channelId !== '0') {
+            $content = \sprintf(
+                'importScripts(\'https://static.cleverpush.com/channel/worker/%s.js\' + self.location.search);',
+                $channelId
+            );
+        }
+
+        return new Response($content, Response::HTTP_OK, ['content-type' => 'text/javascript']);
     }
 }
